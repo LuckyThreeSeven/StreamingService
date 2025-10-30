de81bab189d6a8921ec721ff3d21299d68f3b906
himinqq <alsrud0966@naver.com>
2025-10-30 12:14:12 +0900

    feat:[NVS-262] /processing 폴더로의 파일 이동 시간 메트릭 추가

--- db_upload_server/config.py
+++ db_upload_server/config.py
@@ -11,7 +11,7 @@ COMPLETED_DIR = BASE_DIR / "completed"
 FAILED_DIR = BASE_DIR / "failed"
 
 # 워커 설정
-SCAN_INTERVAL_SECONDS = 10
+SCAN_INTERVAL_SECONDS = 1
 
 # AWS S3 설정
 S3_BUCKET_NAME = os.getenv("S3_BUCKET_NAME", "default-bucket-name")
