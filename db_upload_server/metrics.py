7866a68dcb9195e1abf5c4005e4fa9597377037d
himinqq <alsrud0966@naver.com>
2025-11-05 10:27:50 +0900

    chore: ENV 출력하는 로깅 코드 추가

--- db_upload_server/metrics.py
+++ db_upload_server/metrics.py
@@ -2,6 +2,7 @@ import os
 import logging
 from prometheus_client import start_http_server, Counter, Histogram, Gauge
 
+
 # --- 버킷 로드 헬퍼 ---
 def get_buckets_from_env(env_var_name: str, default_buckets: list[float]) -> list[float]:
     buckets_str = os.getenv(env_var_name)
@@ -23,8 +24,13 @@ def get_buckets_from_env(env_var_name: str, default_buckets: list[float]) -> lis
         )
         return default_buckets
 
+
 # --- 메트릭 정의 ---
-DEFAULT_LATENCY_BUCKETS = [1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2, 3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0, 5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8, 6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6, 8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0]
+DEFAULT_LATENCY_BUCKETS = [1.5, 1.6, 1.7, 1.8, 1.9, 2.0, 2.1, 2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.1, 3.2,
+                           3.3, 3.4, 3.5, 3.6, 3.7, 3.8, 3.9, 4.0, 4.1, 4.2, 4.3, 4.4, 4.5, 4.6, 4.7, 4.8, 4.9, 5.0,
+                           5.1, 5.2, 5.3, 5.4, 5.5, 5.6, 5.7, 5.8, 5.9, 6.0, 6.1, 6.2, 6.3, 6.4, 6.5, 6.6, 6.7, 6.8,
+                           6.9, 7.0, 7.1, 7.2, 7.3, 7.4, 7.5, 7.6, 7.7, 7.8, 7.9, 8.0, 8.1, 8.2, 8.3, 8.4, 8.5, 8.6,
+                           8.7, 8.8, 8.9, 9.0, 9.1, 9.2, 9.3, 9.4, 9.5, 9.6, 9.7, 9.8, 9.9, 10.0]
 latency_buckets = get_buckets_from_env("LATENCY_BUCKETS", DEFAULT_LATENCY_BUCKETS)
 
 # 엔드투엔드 처리 지연 시간 (Histogram)
@@ -41,12 +47,13 @@ THROUGHPUT_COUNTER = Counter(
     ['status']  # 'success' 또는 'failed' 라벨
 )
 
-DEFAULT_SCAN_BUCKETS = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8, 0.85, 0.9, 0.95, 1, 3, 5]
+DEFAULT_SCAN_BUCKETS = [0.01, 0.05, 0.1, 0.15, 0.2, 0.25, 0.3, 0.35, 0.4, 0.45, 0.5, 0.55, 0.6, 0.65, 0.7, 0.75, 0.8,
+                        0.85, 0.9, 0.95, 1, 3, 5]
 scan_buckets = get_buckets_from_env("SCAN_BUCKETS", DEFAULT_SCAN_BUCKETS)
 # EFS/EBS 스캔 자체에 걸린 시간 (Histogram)
 FILE_SCAN_DURATION = Histogram(
-    'video_file_scan_duration_seconds',    # 메트릭 이름은 그대로 사용 (기술 중립적)
-    'Storage /recordings 스캔 작업에 소요된 순수 시간', # 설명에서 'EFS' 제거
+    'video_file_scan_duration_seconds',  # 메트릭 이름은 그대로 사용 (기술 중립적)
+    'Storage /recordings 스캔 작업에 소요된 순수 시간',  # 설명에서 'EFS' 제거
     buckets=scan_buckets
 )
 
@@ -56,7 +63,10 @@ FAILED_FILES_GAUGE = Gauge(
     '현재 /failed 폴더에 쌓여있는 파일의 총 개수'
 )
 
+
 def start_metrics_server():
     """프로메테우스 HTTP 서버 시작"""
     start_http_server(8000)
     logging.info("프로메테우스 서버가 8000번 포트에서 시작되었습니다.")
+    logging.info(f"LATENCY_HISTOGRAM 버킷: {latency_buckets}")
+    logging.info(f"FILE_SCAN_DURATION 버킷: {scan_buckets}")
